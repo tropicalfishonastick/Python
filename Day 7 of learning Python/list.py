c86079cc7f6b58237def8ef19a5a581fa8b24533
Ayushi Tripathi <disisayushi097@gmail.com>
2023-08-27 04:56:40 +0530

    Update List.py
    Python

--- Day 7 of learning Python/list.py
+++ Day 7 of learning Python/list.py
@@ -242,3 +242,21 @@ appends each new element'''
 
 squares = [value**2 for value in range(1, 11)]
 print(squares)
+
+# Slicing a List-------------------------------------------------------------------------------------------------
+'''To make a slice, you specify the index of the first and last elements you want
+to work with'''
+
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(players[0:3])
+print(players[1:4])
+print(players[:4])
+print(players[2:])
+print(players[-3:])
+
+
+# Looping Through a Slice----------------------------------------------------------------------------------------------
+
+print("Here are the first three players on my team:")
+for player in players[:3]:
+   print(player.title())
