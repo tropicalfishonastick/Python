64a9de17c4595af7d06e7d3a489a5167b543f43c
Ayushi Tripathi <disisayushi097@gmail.com>
2023-08-29 10:57:44 +0530

    Update.py
    Update

---
+++ Day 9 of learning Python/exercise11.py
@@ -0,0 +1,25 @@
+'''Favorite Fruit: Make a list of your favorite fruits, and then write a series of
+independent if statements that check for certain fruits in your list.
+• Make a list of your three favorite fruits and call it favorite_fruits.
+• Write five if statements. Each should check whether a certain kind of fruit
+is in your list. If the fruit is in your list, the if block should print a statement,
+such as You really like bananas!'''
+
+# List of favorite fruits
+favorite_fruits = ['banana', 'strawberry', 'mango']
+
+# Check for favorite fruits using if statements
+if 'banana' in favorite_fruits:
+    print("You really like bananas!")
+
+if 'apple' in favorite_fruits:
+    print("You really like apples!")
+
+if 'strawberry' in favorite_fruits:
+    print("You really like strawberries!")
+
+if 'kiwi' in favorite_fruits:
+    print("You really like kiwis!")
+
+if 'mango' in favorite_fruits:
+    print("You really like mangoes!")
--- Day 9 of learning Python/ifstatements.py
+++ Day 9 of learning Python/ifstatements.py
@@ -134,3 +134,74 @@ banned_users = ['andrew', 'carolina', 'david']
 user = 'marie'
 if user not in banned_users:
    print(f"{user.title()}, you can post a response if you wish.")
+
+# IF Statements--------------------------------------------------------------------------------
+# Simple If Statements:
+age = 19
+if age >= 18:
+    print("You are old enough to vote!")
+
+# if-else Statements:
+age = 17
+if age >= 18:
+    print("You are old enough to vote!")
+    print("Have you registered to vote yet?")
+else:
+    print("Sorry, you are too young to vote.")
+    print("Please register to vote as soon as you turn 18!")
+
+# if-elif-else chain
+age = 12
+
+if age < 4:
+    print("Your admission cost is $0.")
+elif age < 18:
+    print("Your admission cost is $25.")
+else:
+    print("Your admission cost is $40.")
+
+# another way to write above code:
+age = 12
+if age < 4:
+   price = 0
+elif age < 18:
+   price = 25
+else:
+   price = 40
+print(f"Your admission cost is ${price}.")
+
+
+# Using Multiple elif Blocks:
+age = 12
+if age < 4:
+   price = 0
+elif age < 18:
+   price = 25
+elif age < 65:
+   price = 40
+else:
+   price = 20
+print(f"Your admission cost is ${price}.")
+
+# Omitting the else Block:
+age = 12
+if age < 4:
+   price = 0
+elif age < 18:
+   price = 25
+elif age < 65:
+   price = 40
+elif age >= 65:
+   price = 20
+print(f"Your admission cost is ${price}.")
+
+
+# Testing Multiple Conditions
+requested_toppings = ['mushrooms', 'extra cheese']
+if 'mushrooms' in requested_toppings:
+   print("Adding mushrooms.")
+if 'pepperoni' in requested_toppings:
+   print("Adding pepperoni.")
+if 'extra cheese' in requested_toppings:
+   print("Adding extra cheese.")
+print("\nFinished making your pizza!")
