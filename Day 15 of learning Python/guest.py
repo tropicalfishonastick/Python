aaeee7028caacd7afce2bf343a0706eb2a2364f4
Ayushi Tripathi <chibi060797@gmail.com>
2023-09-07 15:10:27 +0530

    Update.py
    Update

--- Day 15 of learning Python/guest.py
+++ Day 15 of learning Python/guest.py
@@ -1,7 +1,5 @@
 '''Guest: Write a program that prompts the user for their name. When they respond, write their name to a file called guest.txt.
-Guest Book: Write a while loop that prompts users for their name. Collect all the names that are entered, and 
-then write these names to a file called guest_book.txt. Make sure each entry appears on a new line in the file.'''
-
+'''
 # Prompt the user for their name
 user_name = input("Please enter your name: ")
 
@@ -9,8 +7,11 @@ user_name = input("Please enter your name: ")
 with open("guest.txt", "w") as file:
     file.write(user_name)
 
-'''This program prompts the user for their name, reads the input, and then writes the name to a file called guest.txt.'''
+# This program prompts the user for their name, reads the input, and then writes the name to a file called guest.txt.
 
+'''Guest Book: Write a while loop that prompts users for their name. Collect all the names that are entered, and 
+then write these names to a file called guest_book.txt. Make sure each entry appears on a new line in the file.
+'''
 # Create an empty list to store guest names
 guest_names = []
 
@@ -31,7 +32,7 @@ with open("guest_book.txt", "w") as file:
         file.write(name + "\n")
 
 
-'''This program uses a while loop to continuously prompt users for their names and collects them in a list. 
-When the user enters "quit," the loop exits, and it writes the collected names to a file called guest_book.txt, with each name on a new line.
-Both programs use the with open(...) statement to ensure proper file handling by automatically closing the file when done.
-Adjust the file names as needed for your use case.'''
+# This program uses a while loop to continuously prompt users for their names and collects them in a list. 
+# When the user enters "quit," the loop exits, and it writes the collected names to a file called guest_book.txt, with each name on a new line.
+# Both programs use the with open(...) statement to ensure proper file handling by automatically closing the file when done.
+# Adjust the file names as needed for your use case.
